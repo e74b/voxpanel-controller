a31a17da3dc6100d114c0e73f4f57c7c78e1d21d
Joel Singh <joelebenezersingh@gmail.com>
2026-06-20 04:41:13 +0530

    Documentation and file refactoring

--- auth/__init__.py
+++ auth/__init__.py
@@ -2,7 +2,7 @@ from .tables import User, Scope
 from asyncpg.exceptions import UniqueViolationError
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from commons import success_short, error_short, APIResponse 
+from commons import success_short, error_short, APIResponse
 from typing import Annotated, List
 from .scopes import has_perm, AuthScope, scope_docs
 import hashlib
@@ -14,18 +14,34 @@ OSL_TOKEN = "28a42ae5c72119d32ba65399699c26b903ee4ffc431607997b92e02b0d246b5d"
 router = APIRouter(prefix="/auth", tags=["auth"])
 oauth = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-class LoginForm(BaseModel):
+## PUT /signup
+
+class SignupForm(BaseModel):
     username: str
     password: str
 
-@router.post("/signup", response_model=APIResponse)
-async def create_user(form: LoginForm):
+signup_responses = {
+        201: {
+            "description": "User successfully created",
+            "model": APIResponse
+            },
+        409: {
+            "description": "User aldready exists",
+            "model": APIResponse
+            }
+        }
+
+@router.put("/signup", responses=signup_responses)
+async def create_user(form: SignupForm):
+    """Create a new user with no permissions."""
     user = User(username=form.username, password=form.password)
     try:
         await user.insert(user)
-        return success_short()
+        return success_short(code=201)
     except:
-        return error_short("user exists")
+        return error_short("user exists", code=409)
+
+# POST /login
 
 class LoginSuccessResponseForm(APIResponse):
     token_type: str
@@ -58,6 +74,8 @@ async def login_user(form: Annotated[OAuth2PasswordRequestForm, Depends()]):
     # TODO: Set iat and exp token
     token = jwt.encode(token_data, OSL_TOKEN)
     return success_short(token_type="bearer", access_token=token, **token_data)
+
+# GET /scopes
 
 class TokenData:
     username: str
