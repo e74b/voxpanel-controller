9cd447c8a3cb2f471d2dbe6fcd921bc5731883c7
Joel Singh <joelebenezersingh@gmail.com>
2026-06-26 20:14:31 +0530

    setup script: new Permission instead of old AuthScope

--- setup_users.py
+++ setup_users.py
@@ -1,11 +1,11 @@
-from auth import User, Scope, AuthScope
+from auth import User, Scope, Permission 
 import asyncio
 
 async def setup_users_main():
     u = User(username="admin", password="default")
     await User.insert(u)
 
-    admin_scope = Scope(user=u, scope=AuthScope.Admin.ADMIN)
+    admin_scope = Scope(user=u, scope=Permission.ADMIN)
     await Scope.insert(admin_scope)
 
 asyncio.run(setup_users_main())
